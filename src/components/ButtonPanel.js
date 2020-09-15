0a7878180561deea3dfd48c0ba4457103202e3c7
sergiomauz <sergio.mauz88@gmail.com>
2020-09-14 19:02:17 -0500

    Added classes for displaying keyboard and result

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from './Button';
+
+const ButtonPanel = () => {
+  const panelKeys = {
+    1: ['AC', '+/-', '%', '÷'],
+    2: ['7', '8', '9', 'X'],
+    3: ['4', '5', '6', '-'],
+    4: ['1', '2', '3', '+'],
+    5: ['0', '.', '='],
+  };
+  const panelTag = [];
+  Object.keys(panelKeys).forEach(key => {
+    panelTag.push(
+      <div key={`row_${key}`}>
+        {
+          panelKeys[key].map(btn => (
+            <Button
+              key={`btn_${btn}`}
+              buttonName={btn}
+            />
+          ))
+        }
+      </div>,
+    );
+  });
+
+  return (
+    <div>
+      {panelTag}
+    </div>
+
+  );
+};
+
+export default ButtonPanel;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Big from 'big.js';
-
-const squareRootOfTwo = new Big(1.414213);
-const simpleMessage = `The square root of the number 2 rounded to 6 decimals is: ${squareRootOfTwo}`;
+import App from './components/App';
 
 ReactDOM.render(
-  <h3>{simpleMessage}</h3>,
+  <App />,
   document.getElementById('root'),
 );
